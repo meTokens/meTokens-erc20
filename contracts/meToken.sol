687b4fb5640f48cd993e6e1b8e6036e52d3aa1df
Uni <cryptounico@gmail.com>
2021-05-03 18:57:58 -0500

    update: onlyOwner

--- contracts/meToken.sol
+++ contracts/meToken.sol
@@ -3,11 +3,12 @@
 pragma solidity >=0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 // MeToken with Governance.
-contract MeToken is ERC20('Me Token', 'ME') {
+contract MeToken is ERC20('Me Token', 'ME'), Ownable {
     /// @dev Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
-    function mint(address _to, uint256 _amount) public {
+    function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
     }
