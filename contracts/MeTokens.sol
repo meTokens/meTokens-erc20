46164d97862ec80880e8c55e273f1c05cfa8faa9
Carter Carlson <cartercarlson@protonmail.com>
2022-03-03 10:44:12 -0800

    feat: votes, snapshot

--- contracts/MeTokens.sol
+++ contracts/MeTokens.sol
@@ -1,227 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import {ERC20, IERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {ERC20Burnable} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import {ERC20Snapshot} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol';
+import {ERC20Votes} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
+import {IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol';
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+import {EIP712} from '@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol';
 
-import './IMeTokens.sol';
 
 
 /// @title meTokens erc20 contract (ME)
 /// @author @CBobRobison, @carlfarterson, @cryptounico
 /// @notice Base erc20 contract used for meTokens protocol
-contract MeTokens is ERC20('meTokens', 'ME'), ERC20Burnable, Ownable, IMeTokens {
-
-    /// @notice The EIP-712 typehash for the contract's domain
-    bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
-
-    /// @notice The EIP-712 typehash for the delegation struct used by the contract
-    bytes32 public constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
+abstract contract MeTokens is ERC20Votes, ERC20Snapshot, ERC20Burnable, Ownable {
 
     /// @notice The timestamp after which minting may occur
     uint256 public mintingAllowedAfter;
 
-    /// @notice A checkpoint for marking number of votes from a given block
-    struct Checkpoint {
-        uint32 fromBlock;
-        uint256 votes;
-    }
-
-    /// @notice record of each accounts delegate
-    mapping (address => address) internal _delegates;
-
-
-    /// @notice A record of votes checkpoints for each account, by index
-    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
-
-    /// @notice The number of checkpoints for each account
-    mapping (address => uint32) public numCheckpoints;
-
-
-    /// @notice A record of states for signing / validating signatures
-    mapping (address => uint) public nonces;
-
-
-    constructor() {
+    constructor() ERC20("meTokens", "ME") {
         mint(msg.sender, 1000000 * 10**18); // 1 million ME
         mintingAllowedAfter = block.timestamp + 365 days;
     }
 
 
-    /// @inheritdoc IMeTokens
-    function mint(address _to, uint256 _amount) public onlyOwner override {
+    function mint(address _to, uint256 _amount) public onlyOwner {
         require(block.timestamp >= mintingAllowedAfter, "ME::mint: minting not allowed yet");
         
         _mint(_to, _amount);
-        _moveDelegates(address(0), _delegates[_to], _amount);
-    }
-    
-    /// @inheritdoc IMeTokens
-    function delegates(address delegator) external view override returns (address) {
-        return _delegates[delegator];
     }
 
-    /// @inheritdoc IMeTokens
-    function delegate(address delegatee) external override {
-        return _delegate(msg.sender, delegatee);
+    function getChainId() external view returns (uint256) {
+        return block.chainid;
     }
 
-    /// @inheritdoc IMeTokens
-    function delegateBySig(
-        address delegatee,
-        uint nonce,
-        uint expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    )
-        external override
-    {
-        bytes32 domainSeparator = keccak256(
-            abi.encode(
-                DOMAIN_TYPEHASH,
-                keccak256(bytes(name())),
-                getChainId(),
-                address(this)
-            )
-        );
-
-        bytes32 structHash = keccak256(
-            abi.encode(
-                DELEGATION_TYPEHASH,
-                delegatee,
-                nonce,
-                expiry
-            )
-        );
-
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                domainSeparator,
-                structHash
-            )
-        );
-
-        address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "ME::delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "ME::delegateBySig: invalid nonce");
-        require(block.timestamp <= expiry, "ME::delegateBySig: signature expired");
-        return _delegate(signatory, delegatee);
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        return super._mint(account, amount);
+    }
+    function _burn(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        return super._burn(account, amount);
+    }
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        return super._afterTokenTransfer(from, to, amount);
     }
 
-    /// @inheritdoc IMeTokens
-    function getCurrentVotes(address account)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        uint32 nCheckpoints = numCheckpoints[account];
-        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
-    }
-
-    /// @inheritdoc IMeTokens
-    function getPriorVotes(address account, uint blockNumber)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        require(blockNumber < block.number, "ME::getPriorVotes: not yet determined");
-
-        uint32 nCheckpoints = numCheckpoints[account];
-        if (nCheckpoints == 0) {
-            return 0;
-        }
-
-        // First check most recent balance
-        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
-            return checkpoints[account][nCheckpoints - 1].votes;
-        }
-
-        // Next check implicit zero balance
-        if (checkpoints[account][0].fromBlock > blockNumber) {
-            return 0;
-        }
-
-        uint32 lower = 0;
-        uint32 upper = nCheckpoints - 1;
-        while (upper > lower) {
-            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
-            Checkpoint memory cp = checkpoints[account][center];
-            if (cp.fromBlock == blockNumber) {
-                return cp.votes;
-            } else if (cp.fromBlock < blockNumber) {
-                lower = center;
-            } else {
-                upper = center - 1;
-            }
-        }
-        return checkpoints[account][lower].votes;
-    }
-
-    function _delegate(address delegator, address delegatee)
-        internal
-    {
-        address currentDelegate = _delegates[delegator];
-        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying MEs (not scaled);
-        _delegates[delegator] = delegatee;
-
-        emit DelegateChanged(delegator, currentDelegate, delegatee);
-
-        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
-    }
-
-    function _moveDelegates(address srcRep, address dstRep, uint256 amount) internal {
-        if (srcRep != dstRep && amount > 0) {
-            if (srcRep != address(0)) {
-                // decrease old representative
-                uint32 srcRepNum = numCheckpoints[srcRep];
-                uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint256 srcRepNew = srcRepOld - amount;
-                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
-            }
-
-            if (dstRep != address(0)) {
-                // increase new representative
-                uint32 dstRepNum = numCheckpoints[dstRep];
-                uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint256 dstRepNew = dstRepOld + amount;
-                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
-            }
-        }
-    }
-
-    function _writeCheckpoint(
-        address delegatee,
-        uint32 nCheckpoints,
-        uint256 oldVotes,
-        uint256 newVotes
-    )
-        internal
-    {
-        uint32 blockNumber = safe32(block.number, "ME::_writeCheckpoint: block number exceeds 32 bits");
-
-        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
-            checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
-        } else {
-            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
-            numCheckpoints[delegatee] = nCheckpoints + 1;
-        }
-
-        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
-    }
-
-    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
-        require(n < 2**32, errorMessage);
-        return uint32(n);
-    }
-
-    function getChainId() internal view returns (uint) {
-        uint256 chainId;
-        assembly { chainId := chainid() }
-        return chainId;
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Snapshot) {
+        return super._beforeTokenTransfer(from, to, amount);
     }
 }
