64b86bdf4556e2afe13c9b9b4a4be08c67896888
Carter Carlson <cartercarlson@protonmail.com>
2022-03-04 15:08:50 -0800

    feat: inflation

--- contracts/MeTokens.sol
+++ contracts/MeTokens.sol
@@ -16,23 +16,38 @@ import {EIP712} from '@openzeppelin/contracts/utils/cryptography/draft-EIP712.so
 /// @notice Base erc20 contract used for meTokens protocol
 abstract contract MeTokens is ERC20Votes, ERC20Snapshot, ERC20Burnable, Ownable {
 
-    /// @notice The timestamp after which minting may occur
-    uint256 public mintingAllowedAfter;
+    uint256 constant PRECISION = 10e18;
+    uint256 constant MAX_PCT_MINTABLE = 5 * 10e16; // 5%
+    uint256 private lastMintTimestamp;
+    uint256 private lastMintPct;
 
     constructor() ERC20("meTokens", "ME") {
         mint(msg.sender, 1000000 * 10**18); // 1 million ME
-        mintingAllowedAfter = block.timestamp + 365 days;
+        lastMintTimestamp = block.timestamp;
     }
 
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
-        require(block.timestamp >= mintingAllowedAfter, "ME::mint: minting not allowed yet");
+    function mint(address account, uint256 amount) public onlyOwner {
+        uint256 amountPct = PRECISION * amount / totalSupply();
+        require(amountPct <= getPctMintable(), "amount exceeds max");
         
-        _mint(_to, _amount);
+        lastMintTimestamp = block.timestamp;
+        lastMintPct = getPctMintable() - amountPct;
+
+        _mint(account, amount);
     }
 
     function getChainId() external view returns (uint256) {
         return block.chainid;
+    }
+
+    function getPctMintable() public view returns (uint256) {
+        uint256 period = block.timestamp - lastMintTimestamp;
+        uint256 pctOfYear = PRECISION * period / 365 days;
+        uint256 pctUnlockedToMint = MAX_PCT_MINTABLE * pctOfYear;
+        uint256 totalPctToMint = pctUnlockedToMint + lastMintPct;
+
+        return totalPctToMint >= MAX_PCT_MINTABLE ? MAX_PCT_MINTABLE : totalPctToMint;
     }
 
     function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
